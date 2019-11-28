a08deb0e5b5b0db05044fec8953fc6b1914f42bd
CHAE-YC <yunchang.chae@gmail.com>
2019-11-28 18:24:18 +0900

    navbar.js moved to header.js

---
+++ Tips.md
@@ -0,0 +1,25 @@
+## Objection Model
+* Instance Data Model -- written by yunchang refer from https://json-schema.org/draft/2019-09/json-schema-core.html
+- null: A JSON "null" production
+- boolean: A "true" or "false" value, from the JSON "true" or "false" productions
+- object: An unordered set of properties mapping a string to an instance, from the JSON "object" production
+- array: An ordered list of instances, from the JSON "array" production
+- number: An arbitrary-precision, base-10 decimal number value, from the JSON "number" production
+- string: A string of Unicode code points, from the JSON "string" production
+***
+## Poster URL
+* TMDB API poster size options -- written by yunchang refer from https://www.themoviedb.org/talk/5aeaaf56c3a3682ddf0010de
+- https://image.tmdb.org/t/p/w92/
+- https://image.tmdb.org/t/p/w154/
+- https://image.tmdb.org/t/p/w185/
+- https://image.tmdb.org/t/p/w342/
+- https://image.tmdb.org/t/p/w500/
+- https://image.tmdb.org/t/p/w780/
+- https://image.tmdb.org/t/p/original/
+
+* USAGE
+```
+<% var imgurl = "http://image.tmdb.org/t/p/w780"+movieDetails[i]['movie_posterURL']; %>  // Note: Don't put '/' after the prefix url
+<img alt='Poster' src=<%= imgurl %> />
+```
+***
--- models/Movie.js
+++ models/Movie.js
@@ -1,52 +1,14 @@
 const { BaseModel } = require("./BaseModel");
 class Movie extends BaseModel {
 
-  // constructor(movie_title, movie_genre, movie_rating, movie_runtime, movie_director,
-  //   movie_actors, movie_plot, movie_isAdult, movie_posterURL){
-  //     this.movie_title = movie_title;
-  //     this.movie_genre = movie_genre;
-  //     this.movie_rating = movie_rating;
-  //     this.movie_runtime = movie_runtime;
-  //     this.movie_director = movie_director;
-  //     this.movie_actors = movie_actors;
-  //     this.movie_plot = movie_plot;
-  //     this.movie_isAdult = movie_isAdult;
-  //     this.movie_posterURL = movie_posterURL;
-  // }
-
-  // Table name is the only required property.
   static get tableName() {
     return "movie";
   }
 
-  // Each model must have a column (or a set of columns) that uniquely
-  // identifies the rows. The column(s) can be specified using the `idColumn`
-  // property. `idColumn` returns `id` by default and doesn't need to be
-  // specified unless the model's primary key is something else.
   static get idColumn() {
     return "movie_id";
   }
-
-  // Methods can be defined for model classes just as you would for
-  // any JavaScript class. If you want to include the result of these
-  // method in the output json, see `virtualAttributes`.
-  //   fullName() {
-  //     return this.firstName + ' ' + this.lastName;
-  //   }
-
-  // Optional JSON schema. This is not the database schema!
-  // No tables or columns are generated based on this. This is only
-  // used for input validation. Whenever a model instance is created
-  // either explicitly or implicitly it is checked against this schema.
-  // See http://json-schema.org/ for more info.
   
-  // Instance Data Model -- written by yunchang refer from https://json-schema.org/draft/2019-09/json-schema-core.html
-  // null: A JSON "null" production
-  // boolean: A "true" or "false" value, from the JSON "true" or "false" productions
-  // object: An unordered set of properties mapping a string to an instance, from the JSON "object" production
-  // array: An ordered list of instances, from the JSON "array" production
-  // number: An arbitrary-precision, base-10 decimal number value, from the JSON "number" production
-  // string: A string of Unicode code points, from the JSON "string" production
   static get jsonSchema() {
     return {
       type: "object",
@@ -64,15 +26,7 @@ class Movie extends BaseModel {
         movie_actors: { type: "string", maxLength: 500},
         movie_plot: { type: "string", maxLength: 1000},
         movie_isAdult: {type: "boolean"},
-        // movie_rated: { type: "string", maxLength: 10},
         movie_posterURL: { type: "string", maxLength: 255},
-
-        // Properties defined as objects or arrays are
-        // automatically converted to JSON strings when
-        // writing to database and back to objects and arrays
-        // when reading from database. To override this
-        // behaviour, you can override the
-        // Model.jsonAttributes property.
       }
     };
   }
