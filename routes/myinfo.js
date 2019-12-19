3f67dcda3d09deff27b441362e104fc2aff4907e
jjh <j.h.jeong@outlook.com>
2019-12-19 11:09:49 +0900

    MyInfo date improvements

--- routes/myinfo.js
+++ routes/myinfo.js
@@ -6,6 +6,30 @@ const { raw } = require("objection");
 
 const router = express.Router();
 
+function dateYMD(obj) {
+  // Date
+  if (!obj) return null;
+  return (
+    obj.getUTCFullYear() + "-" + obj.getUTCMonth() + "-" + obj.getUTCDate()
+  );
+}
+
+function dateYMDHM(obj) {
+  // Date
+  if (!obj) return null;
+  return (
+    obj.getUTCFullYear() +
+    "-" +
+    obj.getUTCMonth() +
+    "-" +
+    obj.getUTCDate() +
+    " " +
+    obj.getUTCHours() +
+    ":" +
+    obj.getUTCMinutes()
+  );
+}
+
 router.get("/", async (req, res) => {
   sess = req.session;
 
@@ -46,13 +70,18 @@ router.get("/", async (req, res) => {
     .joinRelated("ticket")
     .joinRelated("ticket.movietimeslot");
 
+  user["user_birthdate"] = dateYMD(user["user_birthdate"]);
+  for (var i = 0; i < tickets5.length; ++i) {
+    tickets5[i]["start_date"] = dateYMDHM(tickets5[i]["start_date"]);
+  }
+
   console.log(user);
   console.log(tickets5);
 
   res.render("myinfo", {
     pagetitle: "My Info",
     pagecss: "myinfo.css",
-    // pagejs: 'myinfo.js',
+    pagejs: "myinfo.js",
     tickets: tickets5,
     user: user,
     username: sess.username
